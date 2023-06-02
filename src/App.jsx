fe7db332749852ebb71b523b01003c531474fbe5
Liliana Nóbrega <lilisabrina2009@gmail.com>
2023-06-02 15:06:08 -0400

    implementación con firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,81 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import TodoList from './components/TodoList/TodoList'
 import TodoForm from './components/TodoForm/TodoForm'
+import { db } from './firebase/firebaseConfig';
+import { collection, getDocs, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 
 function App() {
-  const [todos, setTodos] = useState(
-  [  {
-    'id': '0', 'completed': true, 'task': 'Llorar' 
-  },
-  {
-    'id': '1', 'completed': true, 'task': 'Estudiar'
-  },
-  {
-    'id': '2', 'completed': false, 'task': 'Morir'
-  }
-  ]
-  );
-  
-  
-  useEffect(()=>{
+  const [todos, setTodos] = useState([]);
+  //const [loading, setLoading] = useState(true);
+  const [refresh, setRefresh] = useState(0);
+
+  async function getTasks() {
     //Leer de firestore
-
-  }, []);
-
-
-  const onComplete = (todo) => {
     
-    todo = { ...todo, completed: !todo.completed};
-    const newTodos = todos.map((td) => (td.id === todo.id ? todo : td));
-    setTodos(newTodos);
+    try{
+      //setLoading(true);
+      const querySnapshot = await getDocs(collection(db, "tareas"));
+      let tasks = [];
+      querySnapshot.forEach((tarea) => {
+        const task = {...tarea.data(), 'id': tarea.id};
+        tasks.push(task);
+        })
+      setTodos(tasks);
+      //setLoading(false);
+
+    }catch (e){
+      console.error(e);
+    }
+  }
+
+  useEffect(()=> {
+    getTasks()
+  }, [refresh]);
+
+
+  const onComplete = async (todo) => {
+    
+    //todo = { ...todo, completed: !todo.completed};
+    //const newTodos = todos.map((td) => (td.id === todo.id ? todo : td));
+    //setTodos(newTodos);
+
 
     /*Modificar valor en firestore */
+    await updateDoc(doc(db, "tareas", todo.id), {completed: !todo.completed});
+
+    setRefresh(refresh+1);
   }
 
   
-  const onDelete = (todo) => {
-    setTodos([...todos].filter(td => td.id !== todo.id));
+  const onDelete = async (todo) => {
+    //setTodos([...todos].filter(td => td.id !== todo.id));
+    
     /*Eliminar tarea de firestore */
+    await deleteDoc(doc(db, "tareas", todo.id));
+
+    setRefresh(refresh + 1);
   }
 
-  const onAdd = (task) => {
+  const onAdd = async (task) => {
     const newTask = {'completed': false, 'task': task};
-    newTask.id = todos.length; //Esto no es necesario para firestore
-    const newTodos = [...todos, newTask]; //Esto no es necesario para firestore
-    setTodos(newTodos); //Esto no es necesario para firestore
+    
     /*Agregar a firestore */
+    try {
+      await addDoc(collection(db, "tareas"), newTask);
+    } catch (e) {
+      console.error(e);
+    }
+    setRefresh(refresh + 1);
   }
 
   return (
     <div className="container">
       <h1>To-Do List</h1>
       <TodoForm onAdd={onAdd}/>
+      {//loading ? <div style={{marginTop: '50px', color: '#213547'}}>Loading...</div> : <TodoList todos={todos} onComplete={onComplete} onDelete={onDelete}/>}
       <TodoList todos={todos} onComplete={onComplete} onDelete={onDelete}/>
+      }
+      
     </div>
   )
 }
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArgLSTujSkbtloGPsKQmAK5e0bsdI7ZuA",
+  authDomain: "prepa-firebase-prueba.firebaseapp.com",
+  projectId: "prepa-firebase-prueba",
+  storageBucket: "prepa-firebase-prueba.appspot.com",
+  messagingSenderId: "389829168658",
+  appId: "1:389829168658:web:a2774bf10f1d12dbc592cb",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
